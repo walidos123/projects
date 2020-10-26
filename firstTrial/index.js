fcfe8c124857105527d075c31e70ac8b3d3fd8f1
walidos123 <69328393+walidos123@users.noreply.github.com>
2020-10-26 20:32:29 +0300

    Add files via upload

---
+++ firstTrial/index.js
@@ -0,0 +1,34 @@
+// let walidShrayti = 'miboun'
+// let lmwabna = ['Walid', 'Hamza', 'Jappa']
+// let click = 0
+
+// let asba = {
+//     hamza: 33,
+//     walid: 32,
+//     jappa: 30
+// }
+// function renderAge() {
+// console.log(lmwabna[0] + ' 3omrou ' + asba.walid)    
+// }
+// array + index 0 + string 
+// renderAge();
+
+
+// $(document).ready(function() {
+//     $('button#button-1').click(function(e){
+//         e.preventDefault();
+//        $('div.image-container').addClass('hidden');
+//        $('button#button-1').addClass('hidden');
+       
+//        click--
+//        console.log(click);
+//     })
+//     $('button#button-2').click(function(e){
+//         e.preventDefault();
+//        $('div.image-container').removeClass('hidden');
+//        $('button#button-1').removeClass('hidden');
+       
+//     })
+
+
+// });
